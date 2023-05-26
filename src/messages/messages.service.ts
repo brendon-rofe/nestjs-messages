27acb09eae1e467b264f6ca5a4ff1a0c41df900b
Bravo1B9 <brofe93@gmail.com>
2023-05-26 14:13:20 +0000

    Implement messages service methods

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -7,6 +7,16 @@ export class MessagesService {
     this.messagesRepo = new MessagesRepository();
   };
 
-  
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  };
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  };
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  };
 
 };
