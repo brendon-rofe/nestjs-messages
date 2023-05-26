e28a16606b96386ad6e854f4b6f05ace4c0beb6e
Bravo1B9 <brofe93@gmail.com>
2023-05-26 14:00:48 +0000

    Implement find all function

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -3,17 +3,19 @@ import { readFile, writeFile } from "fs/promises";
 export class MessagesRepository {
 
   async findOne(id: string) {
-    const contents = await readFile('messages.json', 'utf-8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages[id];
   };
 
   async findAll() {
-
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
   };
 
   async create() {
-
+    
   };
 
 };
