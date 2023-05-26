c66ef321fbbcdb213085b19327cf3ea597db410a
Bravo1B9 <brofe93@gmail.com>
2023-05-26 14:11:15 +0000

    Initial messages service setup

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,12 @@
+import { MessagesRepository } from "./messages.repository";
+
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  };
+
+  
+
+};
